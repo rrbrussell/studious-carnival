71267babdcb6a769a3f7eb2f6b151e11c5f7a53a
Robert R. Russell <robert@rrbrussell.com>
2025-12-24 23:48:20 -0600

    BROKEN: saving changes for rewrite.
    The percent encoding logic does not handle UTF-8 correctly.
    Saving so that I have the history.

--- src/decode_urlencoded.rs
+++ src/decode_urlencoded.rs
@@ -13,10 +13,53 @@
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
 
-use std::collections::HashMap;
+use std::str::Chars;
+use std::string::String;
 
-pub fn decode_urlencoded(buffer: &[u8]) -> HashMap<String, Vec<u8>> {
-    let mut output: HashMap<String, Vec<u8>> = HashMap::with_capacity(16);
-    
-    return output;
+pub fn decode_urlencoded(input: String) -> Option<String> {
+    let mut output: String = String::with_capacity(input.len());
+    let mut characters: Chars = input.chars();
+    while let Some(x) = characters.next() {
+        if x == '+' {
+            output.push(' ');
+            continue;
+        }
+        if x != '%' {
+            output.push(x);
+            continue;
+        }
+        let first: Option<char> = characters.next();
+        let second: Option<char> = characters.next();
+        let temp_string: String = String::with_capacity(8);
+        match (first, second) {
+            (Some(f), Some(s)) => {
+                // We hope to have a character in hexadecimal format.
+                if !(f.is_ascii_hexdigit() && s.is_ascii_hexdigit()) {
+                    return None;
+                }
+                let mut y: String = String::from(f);
+                y.push(s);
+                let y: u32 = y.parse().unwrap();
+                match char::from_u32(y) {
+                    Some(z) => {
+                        output.push(z);
+                    }
+                    None => {
+                        // Someone percent encoded something that isn't ASCII.
+                        // To send UTF-8 
+                        return None;
+                    }
+                }
+                todo!();
+            }
+            (Some(_), None) => {
+                // The input data is incomplete or improperly formatted.
+                return None;
+            }
+            (None, _) => {
+                // We hit the end of input.
+            }
+        }
+    }
+    return Some(output);
 }
